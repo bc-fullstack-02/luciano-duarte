677c237bf175946b8a770913bb8df76e71bf3acb
lucianod98 <lucianod98@gmail.com>
2022-11-14 19:42:24 -0300

    fixed

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+const server = http.createServer((req,res) => {
+    if (req.url === '/'){
+        console.log(req.headers)
+        res.write('hello from server')
+        res.end()
+    }
+});
+
+server.on('connetcion', (Stream) => {
+    console.log('some one connected')
+
+});
+
+app.listen(2000, () => {
+    console.log("Servidor está rodando em http://localhost:2000")
+});
