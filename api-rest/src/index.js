e61163fbb1035d0a269a81a7942f12e6ebbcd66b
lucianod98 <lucianod98@gmail.com>
2022-12-14 10:19:13 -0300

    update 2.2

--- api-rest/src/index.js
+++
@@ -1,2 +0,0 @@
-const app = require("./app")
-app.listen(3001,() => {console.log("server running in port 3000")})
--- api-rest/src/routes/post.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const {PostController} = require("../controller")
-const router = express.Router()
-router
-    .route("/posts")
-    .get(PostController.list)
-    .post(PostController.create)
-module.exports = router
